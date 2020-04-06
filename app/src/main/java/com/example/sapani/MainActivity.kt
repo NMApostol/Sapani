1b99b3b6a3c97cd40daa05b9db71a4a1dab6a746
Salma <56955587+sxlmx@users.noreply.github.com>
2020-04-06 22:27:47 +0200

    Funktion 1 verbessert --> Eingabe als Dezimalzahl
    Funktion 2 implementiert
    --> maximal 2 Werte können gespeichert werden, wenn ein dritter Wert eingegeben wird, dann werden die letzten 2 Werte gespeichert und der erste Wert in der Tabelle gelöscht
    
    Funktion 3 implementiert
    --> die 2 gespeicherten Werte werden prozentuell miteinander verglichen
    
    Funktion 4 verbessert
    --> wenn die App geöffnet wird, ist noch kein Patient ausgewählt
    --> zum Speichern bzw. Vergleichen von Werten, muss zuerst ein Patient ausgewählt werden
    
    Salma Elshazly, 06.04.2020

--- app/src/main/java/com/example/sapani/MainActivity.kt
+++ app/src/main/java/com/example/sapani/MainActivity.kt
@@ -3,10 +3,7 @@ package com.example.sapani
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,26 +11,60 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val languages = resources.getStringArray(R.array.Patienten)
-
+        val patienten = resources.getStringArray(R.array.Patienten)
         val spinner = findViewById<Spinner>(R.id.dd_patienten)
+        val button = findViewById<Button>(R.id.simpleButton1)
+        val wundGroese = findViewById<EditText>(R.id.editText1)
+        val wunden = mutableListOf<Number>()
+        val prozentAusgabe = findViewById<TextView>(R.id.TextView3)
+
         if (spinner != null) {
             val adapter = ArrayAdapter(this,
-                android.R.layout.simple_spinner_item, languages)
+                android.R.layout.simple_spinner_item, patienten)
             spinner.adapter = adapter
 
             spinner.onItemSelectedListener = object :
                 AdapterView.OnItemSelectedListener {
                 override fun onItemSelected(parent: AdapterView<*>,
                                             view: View, position: Int, id: Long) {
+                    if (patienten[position] != "Auswählen...")
                     Toast.makeText(this@MainActivity,
                         getString(R.string.selected_item) + " " +
-                                "" + languages[position], Toast.LENGTH_SHORT).show()
+                                "" + patienten[position], Toast.LENGTH_SHORT).show()
                 }
 
                 override fun onNothingSelected(parent: AdapterView<*>) {
                     // write code to perform some action
                 }
+            }
+        }
+
+        button.setOnClickListener {
+            if (spinner.selectedItem != patienten[0]) {
+                var wund = wundGroese.getText().toString()
+                if (wund != "") {
+                    var eingabe = wundGroese.getText().toString().toDouble()
+                    println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" + wunden.size)
+                    if (wunden.size == 0) { wunden.add(0, eingabe) }
+                    else if (wunden.size == 1) { wunden.add(1, eingabe) }
+                    else if (wunden.size == 2) {
+                        var wunde = wunden[1]
+                        wunden[0] = wunde
+                        wunden[1] = eingabe
+                    }
+                    if (wunden.size == 2) {
+                        var prozent = ((wunden[1].toDouble().div(wunden[0].toDouble())) * 100)
+                        prozentAusgabe.setText(prozent.toString() + "%")
+                        println("------------------------------------------------------------------------- " + wunden[0] + " | " + wunden[1] + " | " + prozent)
+                    }
+                    println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" + wunden.size)
+                    wundGroese.setText(null)
+                    Toast.makeText(this@MainActivity, "Wundgröße wurde gespeichert.", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this@MainActivity, "Keine Wundgröße eingegeben.", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this@MainActivity, "Keinen Patienten ausgewählt!", Toast.LENGTH_SHORT).show()
             }
         }
     }
